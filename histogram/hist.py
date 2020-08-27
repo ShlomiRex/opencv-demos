0f865bd32355b7621b89af510e69f22efaebce15
Shlomi <vgtvgy1@gmail.com>
2020-08-28 02:34:51 +0300

    Fixed histogram equalization code

--- histogram/hist.py
+++ histogram/hist.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('a.png',0)
+img = cv2.imread('../resources/view.png', 0)
 
 hist,bins = np.histogram(img.flatten(),256,[0,256])
 
